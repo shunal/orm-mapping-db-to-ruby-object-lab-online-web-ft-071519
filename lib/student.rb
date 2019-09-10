afb79005c6ad18404c50bebed244fb70540a4a91
Steven Hunal <stevehunal@gmail.com>
2019-09-10 06:09:18 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -58,7 +58,7 @@ class Student
     sql = <<-SQL
     SELECT * FROM students WHERE grade = 10;
     SQL
-    DB[:conn].execite(sql).map do |row|
+    DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end.first
   end
