33c74b2a9084616a587b671ed05fe2a002717fd2
Steven Hunal <stevehunal@gmail.com>
2019-09-10 05:51:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -20,7 +20,7 @@ class Student
 
   def self.find_by_name(name)
    sql = <<-SQL
-      SELECT * FROM students WHERE name;
+      SELECT * FROM students WHERE name = ?;
     SQL
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
